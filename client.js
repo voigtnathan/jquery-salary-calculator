f55e7b49fdbab033f35c276c94be6da9ff37d47f
Nate Voigt <voigtnathan23@gmail.com>
2019-08-31 17:04:02 -0500

    created files and employee array for initial testing

---
+++ client.js
@@ -0,0 +1,37 @@
+const employees = [
+  {
+    firstName: 'Jenny',
+    lastName: 'Tailya',
+    employeeID: '2405',
+    annualSalary: '47000',
+    jobTitle: 'Marketing'
+  },
+  {
+    firstName: 'Ben',
+    lastName: 'Dover',
+    employeeID: '2405',
+    annualSalary: '51000',
+    jobTitle: 'backroom specialist'
+  },
+  {
+    firstName: 'Mike',
+    lastName: 'Hunt',
+    employeeID: '2405',
+    annualSalary: '47000',
+    jobTitle: 'boss man'
+  },
+  {
+    firstName: 'Anna',
+    lastName: 'Lee',
+    employeeID: '2405',
+    annualSalary: '47000',
+    jobTitle: 'secretary'
+  },
+  
+];
+
+$(document).ready(readyNow);
+
+function readyNow(){
+
+}
