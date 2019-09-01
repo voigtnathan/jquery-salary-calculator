4abb9934428e72b49c6c7a7277564afd37a977da
Nate Voigt <voigtnathan23@gmail.com>
2019-09-01 17:22:32 -0500

    added employee and render functionality

--- client.js
+++ client.js
@@ -4,28 +4,28 @@ const employees = [
     firstName: 'Jenny',
     lastName: 'Tailya',
     employeeID: '2405',
-    annualSalary: '47000',
+    annualSalary: 47000,
     jobTitle: 'Marketing'
   },
   {
     firstName: 'Ben',
     lastName: 'Dover',
     employeeID: '2405',
-    annualSalary: '51000',
+    annualSalary: 51000,
     jobTitle: 'backroom specialist'
   },
   {
     firstName: 'Mike',
     lastName: 'Hunt',
     employeeID: '2405',
-    annualSalary: '47000',
+    annualSalary: 47000,
     jobTitle: 'boss man'
   },
   {
     firstName: 'Anna',
     lastName: 'Lee',
     employeeID: '2405',
-    annualSalary: '47000',
+    annualSalary: 47000,
     jobTitle: 'secretary'
   },
   
@@ -36,6 +36,7 @@ $(document).ready(readyNow);
 function readyNow(){
   console.log('in ready now')
   handleSubmit();
+  renderList();
 }
 
 function handleSubmit(){
@@ -54,4 +55,29 @@ function addEmployee(){
     jobTitle : $('#jobPosition').val()
   }
   console.log(newEmployee);
+  employees.push(newEmployee);
+  renderList();
+  
 }
+function renderList(){
+  console.log('function to render list');
+  $('#employeeList').empty();
+  for(employee of employees){
+  $('#employeeList').append(`<li>First Name : ${employee.firstName}   Last Name : ${employee.lastName}   ID : ${employee.employeeID}   Salary : ${employee.annualSalary}   Job Title : ${employee.jobTitle}  <button id="deleteEmployee" onCLick="removeEmployee(this)">Delete</button><br/></li>`);
+  }
+  totalSalary();
+}
+
+function totalSalary(){
+  let total = 0;
+  for(let i = 0; i<employees.length; i++){
+    total += employees[i].annualSalary;
+  }
+  $('#totalSalary').empty();
+  $('#totalSalary').append(`Monthly Total Salary = ${total}</h4>`)
+  return total;
+}
+function removeEmployee(){
+
+}
+
