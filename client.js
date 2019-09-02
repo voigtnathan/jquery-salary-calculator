df0ccd3989773d0dc2e3934e132af6d95b89510e
Nate Voigt <voigtnathan23@gmail.com>
2019-09-02 13:45:30 -0500

    added error class and fixed issue with delete button not grabbing correct employee element

--- client.js
+++ client.js
@@ -28,63 +28,74 @@ const employees = [
     annualSalary: 47000,
     jobTitle: 'secretary'
   },
-  
+
 ];
 
 $(document).ready(readyNow);
 
-function readyNow(){
+function readyNow() {
   console.log('in ready now')
   handleSubmit();
   renderList();
 }
 
-function handleSubmit(){
+function handleSubmit() {
   console.log('in handle submit');
   $('#submit').on('click', addEmployee);
-  
+
 }
- 
-function addEmployee(){
-  console.log('in addperson')
+
+function addEmployee() {
+  console.log('in addperson');
+
   let newEmployee = {
-    firstName : $('#first').val(),
-    lastName : $('#last').val(),
-     employeeID : $('#empID').val(),
-     annualSalary : $('#salary').val(),
-    jobTitle : $('#jobPosition').val()
+    firstName: $('#first').val(),
+    lastName: $('#last').val(),
+    employeeID: $('#empID').val(),
+    annualSalary: $('#salary').val(),
+    jobTitle: $('#jobPosition').val()
   }
   console.log(newEmployee);
   employees.push(newEmployee);
   renderList();
 }
 
-function renderList(){
+function renderList() {
   console.log('function to render list');
   $('#employeeList').empty();
-  let objectIndex = 0;
-  for(let i = 0; i < employees.length; i++){
-  $('#employeeList').append(`<li id="employeeObject">First Name : ${employees[i].firstName}   Last Name : ${employees[i].lastName}   ID : ${employees[i].employeeID}   Salary : ${employees[i].annualSalary}   Job Title : ${employees[i].jobTitle}  <button id="delete" onCLick="removeEmployee(this)">Delete</button><br/></li>`);
-  objectIndex += 1;
-}
+  //for (let i = 0; i < employees.length; i++) 
+  for(employee of employees)
+  {
+    let $li = $(`<li id="employeeObject">First Name : ${employee.firstName}  
+      Last Name : ${employee.lastName}  
+      ID : ${employee.employeeID}  
+      Salary : ${employee.annualSalary}  
+      Job Title : ${employee.jobTitle} 
+    <button class="delete">Delete</button><br/></li>`);
+    $li.data('empObjectId', employee.employeeID);
+    $('#employeeList').append($li);
+  }
+  $('.delete').on('click', removeEmployee);
   totalSalary();
 }
 
-function totalSalary(){
+function totalSalary() {
   let total = 0;
-  for(let i = 0; i<employees.length; i++){
+  $('#totalSalary').removeClass('error');
+  for (let i = 0; i < employees.length; i++) {
     total += employees[i].annualSalary;
   }
   $('#totalSalary').empty();
-  $('#totalSalary').append(`Monthly Total Salary = $${total}</h4>`)
-  return total;
+  $('#totalSalary').append(`Monthly Total Salary = $${total}</h4>`);
+  if(total > 20000){
+    $('#totalSalary').addClass('error');
+  }
 }
-function removeEmployee(){
 
-  // console.log('remove employee triggered');
- let toRemove = $(`#employeeObject`).index();
- console.group(toRemove);
-$('#employeeObject').remove();
+function removeEmployee() {
+  //let empObject = $(this).parent().data('empObjectId'); //sets empID data attr to variable and logs to verify
+  //console.log(empObject);
+  $(this).parent().remove();
 
 }
 
